8f2fb62abb93cd0399d2e399904e7afcd7b1e728
Felipe Francisco <fefarias97@gmail.com>
2021-11-20 23:53:04 -0300

    Resolução exercício maior2numeros

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double n1,n2,maior;
+
+Console.Write("Digite o 1º valor: ");
+n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o 2º valor: ");
+n2 = Convert.ToDouble(Console.ReadLine());
+
+if(n1>n2){
+    maior = n1;
+
+}else{
+    maior = n2;
+}
+
+Console.WriteLine($"Maior número é {maior}");
